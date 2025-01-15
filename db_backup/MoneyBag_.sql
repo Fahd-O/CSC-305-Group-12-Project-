05e9c35f6df45064fa59730f81ffe7e24c3608af
Abiola Fahad Ogunnaike <realabiolafahad@gmail.com>
2025-01-15 22:51:56 +0100

    Updated SQL dump for MoneyBag database: changed version, host, and added timestamp to user info table.

--- db_backup/MoneyBag_.sql
+++ db_backup/MoneyBag_.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.3
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:8889
--- Generation Time: Aug 19, 2023 at 12:50 PM
--- Server version: 5.7.26
--- PHP Version: 7.4.2
+-- Host: 127.0.0.1
+-- Generation Time: Jan 15, 2025 at 10:44 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
 SET time_zone = "+00:00";
 
 
@@ -17,41 +18,42 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `MoneyBag.`
+-- Database: `moneybag`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `MoneyBag_Registered_User_Info_Table`
+-- Table structure for table `moneybag_registered_user_info_table`
 --
 
-CREATE TABLE `MoneyBag_Registered_User_Info_Table` (
+CREATE TABLE `moneybag_registered_user_info_table` (
   `idUsers` int(11) NOT NULL,
   `uidUsers` tinytext NOT NULL,
   `emailUsers` tinytext NOT NULL,
-  `pwdUsers` longtext NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='This is where registered user details are stored.';
+  `pwdUsers` longtext NOT NULL,
+  `TimeStamp` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci COMMENT='This is where registered user details are stored.';
 
 --
--- Dumping data for table `MoneyBag_Registered_User_Info_Table`
+-- Dumping data for table `moneybag_registered_user_info_table`
 --
 
-INSERT INTO `MoneyBag_Registered_User_Info_Table` (`idUsers`, `uidUsers`, `emailUsers`, `pwdUsers`) VALUES
-(1, 'Fahd', 'realabiolafahad@gmail.com', '$2y$10$LqjGLRvHu31v571snEhJx.XB1jhn3Z/oMYvcsoyl39tjX5K0MWbv.'),
-(2, '4Fahd', '4realabiolafahad@gmail.com', '$2y$10$63HG0Bu2PQ0MmtVBaw91quqDaQJcrkHa0vlihxAeDLOV3/sedPfaO'),
-(3, 'You', 'you@me.com', '$2y$10$/9oqERSnPvrJ5CxhAc.V/u7C6d4ogys9ppi77ijw0WSdkPINTp2Y6'),
-(4, 'we8', 'we@us.com', '$2y$10$3wdYlH1hy43fv5e0YqoFv.IqfS4gUXro.P7hrvpaGmmSHhpmMvUR6'),
-(5, 'me and you', 'me@you.com', '$2y$10$Ni34IWjsDunCk5h05BnkG.sY3iWITEz.oaAC2Tfvg9DQRn13s3yg.');
+INSERT INTO `moneybag_registered_user_info_table` (`idUsers`, `uidUsers`, `emailUsers`, `pwdUsers`, `TimeStamp`) VALUES
+(1, 'Fahd', 'realabiolafahad@gmail.com', '$2y$10$LqjGLRvHu31v571snEhJx.XB1jhn3Z/oMYvcsoyl39tjX5K0MWbv.', '2025-01-15 21:40:17'),
+(2, '4Fahd', '4realabiolafahad@gmail.com', '$2y$10$63HG0Bu2PQ0MmtVBaw91quqDaQJcrkHa0vlihxAeDLOV3/sedPfaO', '2025-01-15 21:40:17'),
+(3, 'You', 'you@me.com', '$2y$10$/9oqERSnPvrJ5CxhAc.V/u7C6d4ogys9ppi77ijw0WSdkPINTp2Y6', '2025-01-15 21:40:17'),
+(4, 'we8', 'we@us.com', '$2y$10$3wdYlH1hy43fv5e0YqoFv.IqfS4gUXro.P7hrvpaGmmSHhpmMvUR6', '2025-01-15 21:40:17'),
+(5, 'me and you', 'me@you.com', '$2y$10$Ni34IWjsDunCk5h05BnkG.sY3iWITEz.oaAC2Tfvg9DQRn13s3yg.', '2025-01-15 21:40:17');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `MoneyBag_Registered_User_Info_Table`
+-- Indexes for table `moneybag_registered_user_info_table`
 --
-ALTER TABLE `MoneyBag_Registered_User_Info_Table`
+ALTER TABLE `moneybag_registered_user_info_table`
   ADD PRIMARY KEY (`idUsers`);
 
 --
@@ -59,10 +61,11 @@ ALTER TABLE `MoneyBag_Registered_User_Info_Table`
 --
 
 --
--- AUTO_INCREMENT for table `MoneyBag_Registered_User_Info_Table`
+-- AUTO_INCREMENT for table `moneybag_registered_user_info_table`
 --
-ALTER TABLE `MoneyBag_Registered_User_Info_Table`
+ALTER TABLE `moneybag_registered_user_info_table`
   MODIFY `idUsers` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
